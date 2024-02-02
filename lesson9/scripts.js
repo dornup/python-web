c0e85462700ab9b02eb222c700a85267142165b1
Admin <mdiscool1@mail.ru>
2024-02-02 19:13:19 +0700

    idk

---
+++ lesson9/scripts.js
@@ -0,0 +1,22 @@
+//alert()
+
+/*
+блок комментариев
+*/
+
+// 1) ставь ;
+// 2) создавать и изменять переменные по-разному
+console.log(234);
+let a;  // объявление без присваивания (undefined)
+// let a = 123;  // объявление с присваиванием 
+a = '111';  //  изменение
+console.log(a);
+
+let a1 = 11;
+let a2 = 2;
+
+console.log(a1 ** a2);
+console.log(a1 * a2);
+console.log(a1 / a2); // ДЕЛЕНИЕ
+console.log(Math.floor(a1 / a2)); // ЦЕЛОЧИС
+console.log(a1 % a2);
