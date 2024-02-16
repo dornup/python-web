a135f7e8150c806c567ce868fcb30f4191ca4920
Admin <mdiscool1@mail.ru>
2024-02-16 20:41:09 +0700

    сщьше

--- projectmarch/forms.py
+++ projectmarch/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField
 
 class RegisterForm(FlaskForm):
     email = StringField(label='Почта')
+    password = PasswordField(label='Пароль')
     submit = SubmitField(label='Зарегистрироваться')
--- projectmarch/main.py
+++ projectmarch/main.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from forms import RegisterForm
+import os
+SECRET_KEY = os.urandom(32) #генерация уникальной последовательности из 32 байтов
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
 Bootstrap(app)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    # form = RegisterForm()
-    return render_template('header.html', )
+    form = RegisterForm()
+    if request.method == 'POST': # если пользователь что-то отправил
+        if form.validate_on_submit(): #валидация (если форма сказала, что все окей)
+                pass
+        # если пользователь уже занят: почта занята
+        # иначе: хэшируем пароль, создать пользователя в бд, авторизация
+    return render_template('index.html', form=form)
 
 # @app.route("/about")
 # def about_f():
