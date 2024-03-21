aa6a67873a99e7bbb9bbbe255d876659313e045c
Admin <mdiscool1@mail.ru>
2024-03-21 16:51:49 +0700

    yyyyyy

--- projectmarch/static/js/scripts.js
+++ projectmarch/static/js/scripts.js
@@ -4,9 +4,10 @@ let heart_x = 0; let heart_y = 0;
 for (let row = 0; row < 2; row++) {
     for (let column=0; column < 5; column++) {
         let img_tag = document.createElement('img');
-        img_tag.src = '{{ url_for("static", filename="img/logo1.png") }}'
+        img_tag.src = '../static/img/logo1.png'
+        // img_tag.style.left = `${heart_x}px`
         htmlbg_logo.append(img_tag)
-        heart_x += 50;
+        // heart_x += 100;
     }
-    heart_y += 50;
+    // heart_y += 100;
 }
