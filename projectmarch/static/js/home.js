e075cb78c02abc86c0807afbc389db4dccebb864
Admin <mdiscool1@mail.ru>
2024-06-07 23:01:36 +0700

    asdfghjkl;''

--- projectmarch/static/js/home.js
+++ projectmarch/static/js/home.js
@@ -1,5 +1,11 @@
-let meme_click = document.querySelector('.meme_click')
-let shelest_tree = new Audio('../sound/tree.mp3')
+let meme_click = document.querySelector('.meme_click');
+let shelest_tree = new Audio('/static/sound/tree_s.mp3');
+let falling_items = document.querySelector('.falling_items');
 meme_click.addEventListener('click', () => {
-    shelest_tree.play()
-}) 
+    shelest_tree.play();
+
+    let div = document.createElement('div');
+    div.classList.add('apple');
+    div.style.left = `${Math.floor(Math.random() * 98)}vw`;
+    falling_items.appendChild(div)
+});
